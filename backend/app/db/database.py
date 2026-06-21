2a73798c9f53e558ea81dcc18c568bc5ae42dfce
Vijendra Mane <148106020+vijendramane@users.noreply.github.com>
2026-06-21 10:39:10 +0530

    Update database.py

--- backend/app/db/database.py
+++ backend/app/db/database.py
@@ -8,7 +8,7 @@ SQLALCHEMY_DATABASE_URL = settings.DATABASE_URL
 engine = create_engine(SQLALCHEMY_DATABASE_URL)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
-Base = declarative_base()
+Base = declarative_base() 
 
 def get_db():
     db = SessionLocal()
